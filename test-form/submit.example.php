c24b28de2aaa95a41d02fd13ed6af5c425945727
Evan Donovan <evan@techmission.org>
2019-11-26 15:03:45 -0500

    fixed to format of Elementor form

--- test-form/submit.example.php
+++ test-form/submit.example.php
@@ -2,17 +2,24 @@
 
 // Use of ternary operator to set defaults for required parameters
 // isset use: https://phppot.com/php/isset-vs-empty-vs-is_null
-$name = (isset($_POST['name']) ? $_POST['name'] : '');
-$email = (isset($_POST['email']) ? $_POST['email'] : '');
-$phone = (isset($_POST['phone'])? $_POST['phone'] : '');
+// Use of ternary operator to set defaults for required parameters
+// isset use: https://phppot.com/php/isset-vs-empty-vs-is_null
+if(isset($_POST['First_Name']) && isset($_POST['Last_Name'])) {
+	$name = $_POST['First_Name'] . ' ' . $_POST['Last_Name'];
+}
+else {
+	$name = '';
+}
+$email = (isset($_POST['Email']) ? $_POST['Email'] : '');
+$phone = (isset($_POST['Phone'])? $_POST['Phone'] : '');
 
 // Run the node.js command (mind security and permissions settings)
 $command = '/FULL_PATH_TO_SCRIPT/airtable-sync/create.js --name="' . $name . '" --phone="' . $phone . '" --email="' . $email . '"';
 
-echo $command;
+//echo $command;
 
 //die();
 
 $result = shell_exec($command);
 
-var_dump($result);
+//var_dump($result);
